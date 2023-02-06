00e110489ebbd6561a33461dadec7b8d9bcc9698
raashidbaba <raashidbaba849@gmail.com>
2023-02-06 12:19:24 +0530

    added the design.

--- app/src/main/java/com/example/guessit/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guessit/score/ScoreFragment.kt
@@ -1,4 +1,21 @@
 package com.example.guessit.score
 
-class ScoreFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.guessit.R
+import com.example.guessit.databinding.ScoreFragmentBinding
+
+class ScoreFragment :Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<ScoreFragmentBinding>(inflater, R.layout.score_fragment,container,false)
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/guessit/screens/GameFragment.kt
+++ app/src/main/java/com/example/guessit/screens/GameFragment.kt
@@ -1,4 +1,4 @@
-package com.example.guessit.title
+package com.example.guessit.screens
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,20 +7,15 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.guessit.R
-import com.example.guessit.databinding.TitleFragmentBinding
+import com.example.guessit.databinding.GameFragmentBinding
 
-class TitleFragment : Fragment() {
+class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<TitleFragmentBinding>(
-            inflater,
-            R.layout.title_fragment,
-            container,
-            false
-        )
+        val binding = DataBindingUtil.inflate<GameFragmentBinding>(inflater, R.layout.game_fragment,container,false)
         return binding.root
     }
 }
