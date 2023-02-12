15f6cabe8ffbde935525409eca845f3ec821ed90
raashidbaba <raashidbaba849@gmail.com>
2023-02-12 22:20:02 +0530

    fixed rotation issue with ViewModel

--- app/src/main/java/com/example/guessit/screens/GameViewModel.kt
+++ app/src/main/java/com/example/guessit/screens/GameViewModel.kt
@@ -4,12 +4,75 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+     var word = ""
+     var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+
+
     init {
+        resetList()
+        nextWord()
         Log.i("GameModel","GameViewModel created")
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameModel","destroyed")
+    }
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+   private  fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+
+    }
+    /** Methods for buttons presses **/
+
+     fun onSkip() {
+        score--
+        nextWord()
+    }
+
+     fun onCorrect() {
+        score++
+        nextWord()
     }
 }
