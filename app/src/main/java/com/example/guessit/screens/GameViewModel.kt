9c3da85973aacf0c8fd15c473ae44b16792703ee
raashidbaba <raashidbaba849@gmail.com>
2023-02-18 22:56:33 +0530

    Added Encapsulation.

--- app/src/main/java/com/example/guessit/screens/GameViewModel.kt
+++ app/src/main/java/com/example/guessit/screens/GameViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.guessit.screens
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    var word = MutableLiveData<String>()
-    var score = MutableLiveData<Int>()
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+    get() = _word
+
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+    get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -14,8 +20,8 @@ class GameViewModel : ViewModel() {
 
 
     init {
-        score.value = 0
-        word.value = ""
+        _score.value = 0
+        _word.value = ""
         resetList()
         nextWord()
         Log.i("GameModel","GameViewModel created")
@@ -63,19 +69,19 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
     /** Methods for buttons presses **/
 
      fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
      fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 }
