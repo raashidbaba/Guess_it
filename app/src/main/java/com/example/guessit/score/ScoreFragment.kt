44b5c14a50e26d2c6825316a2d75b8d24d1fcdb7
raashidbaba <raashidbaba849@gmail.com>
2023-02-25 23:26:05 +0530

    add ViewModelFactory to score.

--- app/src/main/java/com/example/guessit/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guessit/score/ScoreFragment.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.guessit.R
 import com.example.guessit.databinding.ScoreFragmentBinding
 
 class ScoreFragment :Fragment(){
+    private lateinit var viewModel : ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +25,10 @@ class ScoreFragment :Fragment(){
         val binding = DataBindingUtil.inflate<ScoreFragmentBinding>(inflater, R.layout.score_fragment,container,false)
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+      /*  viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments).score)
+       // viewModel = ViewModelProvider(this,viewModelFactory).get(ScoreViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(ScoreViewModel::class.java)*/
         binding.scoreText.text = scoreFragmentArgs.score.toString()
         binding.playAgainButton.setOnClickListener { onPlayAgain() }
         return binding.root
--- app/src/main/java/com/example/guessit/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/guessit/score/ScoreViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.guessit.score
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class ScoreViewModel(finalScore: Int) {
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    init {
+        _score.value = finalScore
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
 }
